1f7758d8a18fbfffed4c2a80922d6cf786aeb922
SiHyung92 <fjzjqhdl@gmail.com>
2020-03-27 11:18:57 +0900

    feat : Type 순서 추가 및 테스트

--- src/main/java/chess/game/piece/attribute/Type.java
+++ src/main/java/chess/game/piece/attribute/Type.java
@@ -12,33 +12,31 @@ public enum Type {
     ROOK("R"),
     PAWN("P");
 
-    private static List<Type> list = new ArrayList<>();
+    private static List<Type> orderedType;
 
-    private static final int NUMBER_OF_KING = 1;
-    private static final int NUMBER_OF_QUEEN = 1;
-    private static final int NUMBER_OF_BISHOP = 2;
-    private static final int NUMBER_OF_KNIGHT = 2;
-    private static final int NUMBER_OF_ROOK = 2;
-    private static final int NUMBER_OF_PAWN = 6;
-
-    private static final int[] numberOfType =
-            new int[]{NUMBER_OF_KING, NUMBER_OF_QUEEN, NUMBER_OF_BISHOP, NUMBER_OF_KNIGHT, NUMBER_OF_ROOK, NUMBER_OF_PAWN};
+    private static final int NUMBER_OF_PAWN = 8;
+    private static final Type[] typesOrderWithOutPawn =
+            new Type[]{ROOK, KNIGHT, BISHOP, KING, QUEEN, BISHOP, KNIGHT, ROOK};
 
     String initial;
+
+    static {
+        orderedType = Arrays.asList(typesOrderWithOutPawn);
+    }
 
     Type(String initial) {
         this.initial = initial;
     }
 
-    public static List<Type> aSet() {
-        Arrays.stream(values())
-                .forEach(type -> addByEachCount(type, numberOfType[type.ordinal()]));
-        return list;
+    public static List<Type> orderedTypesWithoutPawn() {
+        return orderedType;
     }
 
-    private static void addByEachCount(Type type, int count) {
-        for (int i = 0; i < count; i++) {
-            list.add(type);
+    public static List<Type> pawns() {
+        List<Type> pawns = new ArrayList<>(NUMBER_OF_PAWN);
+        for (int i = 0; i < NUMBER_OF_PAWN; i++) {
+            pawns.add(PAWN);
         }
+        return pawns;
     }
 }
